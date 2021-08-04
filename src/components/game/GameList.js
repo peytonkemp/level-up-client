f2f5447bb7565951b9f335ed96666de9d9a72162
peytonkemp <peyton.kemp@gmail.com>
2021-08-04 09:36:35 -0500

    game list and game provider

---
+++ src/components/game/GameList.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from "react"
+import { GameContext } from "./GameProvider.js"
+
+export const GameList = (props) => {
+    const { games, getGames } = useContext(GameContext)
+
+    useEffect(() => {
+        getGames()
+    }, [])
+
+    return (
+        <article className="games">
+            {
+                games.map(game => {
+                    return <section key={`game--${game.id}`} className="game">
+                        <div className="game__name">{game.name} by {game.maker}</div>
+                        <div className="game__players">{game.number_of_players} players needed</div>
+                    </section>
+                })
+            }
+        </article>
+    )
+}
