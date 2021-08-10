37e96381b562ddb580fec32894f60d532cad77bc
peytonkemp <peyton.kemp@gmail.com>
2021-08-10 09:32:30 -0500

    Game form. Added 'add game' button to game list. Added createGame and getGameTypes to game provider.

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from "react"
-import { GameContext } from "./GameProvider.js"
-
+import { GameContext, } from "./GameProvider.js"
+import { useHistory } from "react-router-dom/cjs/react-router-dom"
 export const GameList = (props) => {
     const { games, getGames } = useContext(GameContext)
+    const history = useHistory()
 
     useEffect(() => {
         getGames()
@@ -10,6 +11,11 @@ export const GameList = (props) => {
 
     return (
         <article className="games">
+            <button className="btn btn-2 btn-sep icon-create"
+                onClick={() => {
+                    history.push({ pathname: "/games/new" })
+                }}
+            >Add New Game</button>
             {
                 games.map(game => {
                     return <section key={`game--${game.id}`} className="game">
